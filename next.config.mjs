f0af422b9230be40e2016e448448d18381ecd8d3
ringgabudiutama <ringgabudiutama819@gmail.com>
2026-08-15 10:04:22 +0700

    Update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
       { protocol: "https", hostname: "*.public.blob.vercel-storage.com" },
     ],
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
